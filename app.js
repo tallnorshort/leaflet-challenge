6811d618fd081bc8017cd86b201e271891cfd959
david.j.brewer28.mil@gmail.com <david.j.brewer28.mil@gmail.com>
2020-06-07 14:37:42 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,160 @@
+
+//Step one: Create variables for maplayers using "L.tileLayer" and an api key token
+
+
+// Selectable backgrounds of our map - tile layers:
+
+
+// grayscale background.
+var graymap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoidGFsbG5vcnNob3J0IiwiYSI6ImNrYW9vYWZ6OTBhcGkyd3AxcnFhdHloMGoifQ.x2d2nLp16h-MRVN4zBN6Mg");
+
+// satellite background.
+var satellitemap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoidGFsbG5vcnNob3J0IiwiYSI6ImNrYW9vYWZ6OTBhcGkyd3AxcnFhdHloMGoifQ.x2d2nLp16h-MRVN4zBN6Mg");
+
+// outdoors background.
+var outdoors_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoidGFsbG5vcnNob3J0IiwiYSI6ImNrYW9vYWZ6OTBhcGkyd3AxcnFhdHloMGoifQ.x2d2nLp16h-MRVN4zBN6Mg");
+
+// map object to an array of layers we created.
+var map = L.map("mapid", { //using the leaflet app (which we call with "L"). "mapid" is our class id we call later in our HTML
+  center: [37.09, -95.71], //starting coornates 
+  zoom: 5, // starting zoom level for map
+  layers: [graymap_background, satellitemap_background, outdoors_background] //various different layer maps from our variables
+});
+
+// adding one 'graymap' tile layer to the map. This function is needed to have the map show up
+graymap_background.addTo(map); 
+
+
+//Step two: Creating the earthquake and tectonicplate maps 
+
+
+// layers for two different sets of data, earthquakes and tectonicplates.
+var tectonicplates = new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
+
+// base layers
+var baseMaps = {
+  Satellite: satellitemap_background,
+  Grayscale: graymap_background,
+  Outdoors: outdoors_background
+};
+
+// overlays 
+var overlayMaps = {
+  "Tectonic Plates": tectonicplates, 
+  "Earthquakes": earthquakes
+};
+
+// control which layers are visible.
+L
+  .control
+  .layers(baseMaps, overlayMaps)
+  .addTo(map);
+
+// retrieve earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(data) { //pulling json data from the website
+
+
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // Define the color of the marker based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    switch (true) {
+      case magnitude > 5:
+        return "#ea2c2c";
+      case magnitude > 4:
+        return "#ea822c";
+      case magnitude > 3:
+        return "#ee9c00";
+      case magnitude > 2:
+        return "#eecc00";
+      case magnitude > 1:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
+    }
+  }
+
+  // define the radius of the earthquake marker based on its magnitude.
+
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 3;
+  }
+
+  // add GeoJSON layer to the map
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+
+  legend.onAdd = function() {
+    var div = L
+      .DomUtil
+      .create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+
+  legend.addTo(map);
+
+  // retrive Tectonic Plate geoJSON data.
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+    function(platedata) {
+ 
+      L.geoJson(platedata, {
+        color: "orange",
+        weight: 2
+      })
+      .addTo(tectonicplates);
+
+      // add the tectonicplates layer to the map.
+      tectonicplates.addTo(map);
+    });
+});
---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidGFsbG5vcnNob3J0IiwiYSI6ImNrYW9vYWZ6OTBhcGkyd3AxcnFhdHloMGoifQ.x2d2nLp16h-MRVN4zBN6Mg";
